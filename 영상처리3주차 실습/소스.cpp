bb2a15f6c8994f391f851b484d9dc1ad8aef9507
HYEWON PARK <hyewon9913@gmail.com>
2022-04-06 15:47:23 +0900

    Histogram Equalization assignment

--- 영상처리3주차 실습/소스.cpp
+++ 영상처리3주차 실습/소스.cpp
@@ -6,7 +6,7 @@ int main() {
 	BITMAPFILEHEADER bmpFile;
 	BITMAPINFOHEADER bmpInfo;
 	FILE* inputFIle1 = NULL;
-	inputFIle1 = fopen("AICenterY_Dark.bmp", "rb");
+	inputFIle1 = fopen("HeOriginal.bmp", "rb");
 	fread(&bmpFile, sizeof(BITMAPFILEHEADER), 1, inputFIle1);
 	fread(&bmpInfo, sizeof(BITMAPINFOHEADER), 1, inputFIle1);
 	FILE* inputFIle2 = NULL;
@@ -20,10 +20,16 @@ int main() {
 	int stride = (((bitCnt / 8) * width) + 3) / 4 * 4;
 
 
+
+
 	unsigned char* inputImg1 = NULL, * outputImg = NULL, * inputImg2 = NULL;
+	unsigned char* HeImg = NULL, * HeImgHisto = NULL, * OriginHisto = NULL;
 	inputImg1 = (unsigned char*)calloc(size, sizeof(unsigned char));
 	inputImg2 = (unsigned char*)calloc(size, sizeof(unsigned char));
 	outputImg = (unsigned char*)calloc(size, sizeof(unsigned char));
+	HeImg = (unsigned char*)calloc(size, sizeof(unsigned char));
+	HeImgHisto = (unsigned char*)calloc(size, sizeof(unsigned char));
+	OriginHisto = (unsigned char*)calloc(size, sizeof(unsigned char));
 	fread(inputImg1, sizeof(unsigned char), size, inputFIle1);
 	fread(inputImg2, sizeof(unsigned char), size, inputFIle2);
 	unsigned char* Y = NULL;
@@ -35,6 +41,9 @@ int main() {
 		for (int i = 0; i < width; i++) {
 			Y[j * width + i] = inputImg1[j * stride + 3 * i + 0];
 			hist[Y[j * width + i]] ++;
+			outputImg[j * stride + 3 * i + 0] = Y[j * width + i];
+			outputImg[j * stride + 3 * i + 1] = Y[j * width + i];
+			outputImg[j * stride + 3 * i + 2] = Y[j * width + i];
 		
 		}
 	}
@@ -71,9 +80,9 @@ int main() {
 			Y3[j * width + i] = 255 * sumhist[Y1[j * width + i]] / (width * height);
 			hist2[Y3[j * width + i]] ++;
 
-			outputImg[j * stride + 3 * i + 0] = Y3[j * width + i];
-			outputImg[j * stride + 3 * i + 1] = Y3[j * width + i];
-			outputImg[j * stride + 3 * i + 2] = Y3[j * width + i];
+			HeImg[j * stride + 3 * i + 0] = Y3[j * width + i];
+			HeImg[j * stride + 3 * i + 1] = Y3[j * width + i];
+			HeImg[j * stride + 3 * i + 2] = Y3[j * width + i];
 		}
 	}
 
@@ -96,18 +105,33 @@ int main() {
 		sumouthist2[j] = sumhist2[j / 2] / 512;
 		sumouthist2[j + 1] = sumhist2[j / 2] / 512;
 	}
+
+	for (int j = 0; j < height; j++) {
+		for (int i = 0; i < width; i++) {
+			if (j <= outhist[i]) {
+				OriginHisto[j * stride + 3 * i + 0] = 0;
+				OriginHisto[j * stride + 3 * i + 1] = 0;
+				OriginHisto[j * stride + 3 * i + 2] = 0;
+			}
+			if (j >= outhist[i]) {
+				OriginHisto[j * stride + 3 * i + 0] = 255;
+				OriginHisto[j * stride + 3 * i + 1] = 255;
+				OriginHisto[j * stride + 3 * i + 2] = 255;
+			}
+		}
+	}
 	
 	for (int j = 0; j < height; j++) {
 		for (int i = 0; i < width; i++) {
-			if (j <= sumouthist2[i]) {
-				outputImg[j * stride + 3 * i + 0] = 0;
-				outputImg[j * stride + 3 * i + 1] = 0;
-				outputImg[j * stride + 3 * i + 2] = 0;
+			if (j <= outhist2[i]) {
+				HeImgHisto[j * stride + 3 * i + 0] = 0;
+				HeImgHisto[j * stride + 3 * i + 1] = 0;
+				HeImgHisto[j * stride + 3 * i + 2] = 0;
 			}
-			if (j >= sumouthist2[i]) {
-				outputImg[j * stride + 3 * i + 0] = 255;
-				outputImg[j * stride + 3 * i + 1] = 255;
-				outputImg[j * stride + 3 * i + 2] = 255;
+			if (j >= outhist2[i]) {
+				HeImgHisto[j * stride + 3 * i + 0] = 255;
+				HeImgHisto[j * stride + 3 * i + 1] = 255;
+				HeImgHisto[j * stride + 3 * i + 2] = 255;
 			}
 		}
 	}
@@ -120,11 +144,29 @@ int main() {
 	fwrite(&bmpFile, sizeof(BITMAPFILEHEADER), 1, outputFile);
 	fwrite(&bmpInfo, sizeof(BITMAPINFOHEADER), 1, outputFile);
 	fwrite(outputImg, sizeof(unsigned char), size, outputFile);
+
+	FILE* outputFile1 = fopen("HeImg.bmp", "wb");
+	fwrite(&bmpFile, sizeof(BITMAPFILEHEADER), 1, outputFile1);
+	fwrite(&bmpInfo, sizeof(BITMAPINFOHEADER), 1, outputFile1);
+	fwrite(HeImg, sizeof(unsigned char), size, outputFile1);
+
+	FILE* outputFile2 = fopen("HeImgHisto.bmp", "wb");
+	fwrite(&bmpFile, sizeof(BITMAPFILEHEADER), 1, outputFile2);
+	fwrite(&bmpInfo, sizeof(BITMAPINFOHEADER), 1, outputFile2);
+	fwrite(HeImgHisto, sizeof(unsigned char), size, outputFile2);
+
+	FILE* outputFile3 = fopen("OriginHisto.bmp", "wb");
+	fwrite(&bmpFile, sizeof(BITMAPFILEHEADER), 1, outputFile3);
+	fwrite(&bmpInfo, sizeof(BITMAPINFOHEADER), 1, outputFile3);
+	fwrite(OriginHisto, sizeof(unsigned char), size, outputFile3);
+
+
 	free(outputImg);
+	fclose(outputFile);
+
 	free(inputImg1);
 	fclose(inputFIle1);
 	free(inputImg2);
 	fclose(inputFIle2);
-	fclose(outputFile);
 	return 0;
 }
